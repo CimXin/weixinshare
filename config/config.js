278d4bbf9ee3fc794ccee889a856c9be91424b06
Mic <372134690@qq.com>
2017-06-05 23:32:23 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    wechat: {
+        token: "wei790",
+        appid: 'wx1172077d3e605de0',
+        appSecret: "c4b497a6b33ea4a3c6caa48293e902e6",
+        encodingAESKey: 'LiUosW5yi2U5OL1owj1rUMJoupamEHocGNOkQT57CcY'
+    },
+
+};
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+'use strict';
+const Api = require("weixin-api");
+const express = require('express');
+const router = express.Router();
+const config = require("../config/config");
+
+const api = new Api(config.wechat.appid, config.wechat.appSecret);
+
+router.get("/getConfig", function (req, res, next) {
+    var params = {
+        debug: true,
+        jsApiList: [
+            "onMenuShareTimeline",
+            "onMenuShareAppMessage"
+        ],
+        url: req.query.url
+    };
+
+    api.getJSConfig(params, function (err, result) {
+        console.log(result);
+        res.send(result);
+    });
+});
+
+router.get("/test", function (req, res, next) {
+    res.send({test: "test"});
+});
+
+module.exports = router;
